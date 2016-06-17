8751d9b129f87cf4a4704e6a7aa132f948544b1a
alex guilbaud <drac21@orange.fr>
2016-06-18 00:32:33 +0200

    Modify load map

--- contract/src/main/java/contract/IModel.java
+++ contract/src/main/java/contract/IModel.java
@@ -5,24 +5,9 @@ import java.util.Observable;
 /**
  * The Interface IModel.
  *
- * @author Jean-Aymeric Diet
+ * @author Jean-Aymeric Diet and modify by Alex GUILBAUD
  */
 public interface IModel {
-
-	/**
-	 * Gets the map.
-	 *
-	 * @return the map
-	 */
-	String getMap();
-
-	/**
-	 * Load the message.
-	 *
-	 * @param id
-	 *          the id
-	 */
-	void loadMap(int id);
 
 	/**
 	 * Gets the observable.
@@ -30,4 +15,22 @@ public interface IModel {
 	 * @return the observable
 	 */
 	Observable getObservable();
+
+
+	/**
+	 * Gets the map content.
+	 *
+	 * @return the content
+	 */
+	String getMap();
+
+	/**
+	 * Load  Map.
+	 *
+	 * @param key
+	 *          the key
+	 */
+	void loadMap(String key);
+
+	IElement element(char c, Point pos);
 }
