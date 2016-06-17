5da238cf1ca690eef6bdf31e6a5a89024fca914e
Mathieu <Mathieu@10.144.50.38>
2016-06-18 00:02:47 +0200

    We add movements and some test maps

--- contract/src/main/java/contract/ControllerOrder.java
+++ contract/src/main/java/contract/ControllerOrder.java
@@ -3,14 +3,24 @@ package contract;
 /**
  * The Enum ControllerOrder.
  *
- * @author Jean-Aymeric Diet
  */
 public enum ControllerOrder {
 
-	/** The English. */
-	Map1, /** The first map. */
-	Map2, /** The second map. */
-	Map3, /** The third map. */
-	Map4, /** The fourth map. */
-	Map5  /** The fifth map.*/
+	MOVEUP,
+	MOVEDOWN,
+	MOVERIGHT,
+	MOVELEFT,
+	MOVELEFTUP,
+	MOVERIGHTUP,
+	MOVERIGHTDOWN,
+	MOVELEFTDOWN,
+	MAP1,/** The first map. */
+	MAP2,/** The second map. */
+	MAP3,/** The third map. */
+	MAP4,/** The fourth map. */
+	MAP5,/** The fifth map.*/
+	MENU,
+	WORKSHOP,
+    FIRE
+
 }
