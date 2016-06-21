c007598159971aed2f3d7eebf1b996360726f46f
alex guilbaud <drac21@orange.fr>
2016-06-21 09:17:18 +0200

    test

---
+++ view/src/test/ViewTest.java
@@ -0,0 +1,74 @@
+package view;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ViewTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testView() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHeight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWidth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKeyCodeToControllerOrder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShowdiemessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWinmessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRepaint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetController() {
+		fail("Not yet implemented");
+	}
+
+}
